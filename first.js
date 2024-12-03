f5edea253836271f7fa245e127419d49f34ded11
OliviaMoras <oliviamoras10@gmail.com>
2024-12-03 18:33:08 +0530

    code added

--- first.js
+++ first.js
@@ -5,3 +5,24 @@ console.log("olivia")
 // v20.10.0
 // PS D:\JavaScript> node first.js
 // olivia;
+
+//Variables
+
+//1. Const
+const id=123
+//id=2   TypeError: Assignment to constant variable.
+// not possible becuase const can not be changed.
+console.log(id)
+
+//2. let
+// Always use Let rather than var cause issue in block and functional 
+let ac_email="oli10@gmail.com"
+
+//3. var
+var acpassword="123"
+
+ac_city="goa"
+
+let acc_state
+
+console.log([id, ac_email,acpassword,ac_city,acc_state ])
