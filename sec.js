19c750f1bb6457b13cc466a0b715f939df002ca5
OliviaMoras <oliviamoras10@gmail.com>
2024-12-03 19:11:00 +0530

    removed some code

--- sec.js
+++ sec.js
@@ -1,9 +1,3 @@
-"use strict"; // treat all JS Code as newer version
-
-//alert(3+3)  we cant use beacuse of node
-
-//number, bigint, string, boolean, null, undefined, symbol, object
-
 console.log(typeof "olivia");
 
 console.log(typeof null);
