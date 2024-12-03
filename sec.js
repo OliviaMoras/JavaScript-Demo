7dae0f338d184d41ef08b35748acde83a12cfb98
OliviaMoras <oliviamoras10@gmail.com>
2024-12-03 20:44:47 +0530

    Added some code

--- sec.js
+++ sec.js
@@ -1,3 +1,5 @@
+//CONVERSION
+
 console.log(typeof "olivia");
 
 console.log(typeof null);
@@ -38,3 +40,27 @@ let num=3
 
 let str_no=String(num)
 console.log(typeof(str_no));  //string
+
+console.log("============= OPERATIONS ==================");
+
+let value =3
+let negvalue = -value
+console.log(negvalue)
+
+console.log("1"+2)   //12
+console.log("1"+2+2)  //122
+console.log(1+2+"2")  //32
+
+console.log(true)
+console.log(+true)  //1
+//console.log(true+)  ERROR 
+console.log(+"")   //0
+
+let counter=100
+++counter;
+console.log(counter);
+
+//Summary
+
+
+
