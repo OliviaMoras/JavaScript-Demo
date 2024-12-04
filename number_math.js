da33b975de5e45e079d1d2bfe016be4cce749ec6
OliviaMoras <oliviamoras10@gmail.com>
2024-12-04 11:39:08 +0530

    string code added

---
+++ number_math.js
@@ -0,0 +1,25 @@
+const score=200
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const num=888.9898
+console.log(num.toPrecision(3));
+const num1=88.9898
+console.log(num1.toPrecision(3));
+
+console.log("-------------MATH---------------");
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.8));
+console.log(Math.floor(4.8));
+console.log(Math.ceil(4.8));
+console.log(Math.min(4.8,5,1,4));
+console.log(Math.max(4.8,5,1,4));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
