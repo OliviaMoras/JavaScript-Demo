307be85f24178c6ffca73f29141af1905fa31e25
OliviaMoras <oliviamoras10@gmail.com>
2024-12-04 12:10:32 +0530

    date time code added

---
+++ date_time.js
@@ -0,0 +1,14 @@
+let mydate= new Date()
+console.log(mydate);
+console.log(mydate.toString());
+console.log(mydate.toDateString());
+console.log(mydate.toISOString());
+console.log(mydate.toLocaleString());
+console.log(mydate.toLocaleDateString());
+console.log(mydate.toTimeString());
+console.log(typeof mydate);                   //object
+
+//month starts from 0
+
+let mytime = Date.now()
+console.log(mytime);
